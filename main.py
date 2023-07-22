13319cf864416d410ebb63f033d4ac47222d3750
Nicole Arwood <nsarwood@gmail.com>
2023-07-22 18:24:33 -0400

    Newer and improveder

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from course import *
 from scraper import *
 
+def printRow(course, group, file):
+    file.write(course.name + "," + group.code + "," + group.dates.__str__() + "," + group.language + "," + course.hours + "\n")
 
 def printAll():
     with open('output.txt', 'w') as f:
@@ -8,7 +10,7 @@ def printAll():
         for row in getAllRows():
             for group in getGroups(row):
                 course = createClass(row)
-                f.write(course.name + "," + group.code + "," + group.dates.__str__() + "," + group.language + "," + course.hours + "\n")
+                printRow(course, group, f)
 
 def filt(langList, daysList, hourList):
     with open('output.txt', 'w') as f:
@@ -21,10 +23,20 @@ def filt(langList, daysList, hourList):
                     if day not in group.days():
                         dayFlag = False
                 if (group.language in langList) and (course.hours in hourList) and dayFlag:
-                    f.write(course.name + "," + group.code + "," + group.dates.__str__() + "," + group.language + "," + course.hours + "\n")
+                    printRow(course, group, f)
+
+def threeDayWeek():
+    with open('output.txt', 'w') as f:
+        for row in getAllRows():
+            for group in getGroups(row):
+                course = createClass(row)
+                if 'Mon' not in group.days() and 'Fri' not in group.days() and course.hours == '3' and group.language == 'English':
+                    printRow(course, group, f)
+
 
 def main():
-    filt(['English'], ['Fri'], ['6'])
+    #filt(['English'], ['Fri'], ['6'])
+    threeDayWeek()
     print("Done")
     
 if __name__ == '__main__':
